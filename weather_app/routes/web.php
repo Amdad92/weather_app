3d8f091defdabc21c6052d82bb1ae08c71041fa5
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-03-13 13:18:45 +0530

    added routes to the web routfile

--- weather_app/routes/web.php
+++ weather_app/routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WeatherController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/weather', [WeatherController::class, 'showWeatherForm']);
 Route::post('/weather', [WeatherController::class, 'getWeather']);
